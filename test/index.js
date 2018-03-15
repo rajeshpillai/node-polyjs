03ab4ba386b7b434b5168575f8914db3c56052b9
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-03-15 14:16:35 +0530

    Basic route test for GET request

--- test/index.js
+++ test/index.js
@@ -3,6 +3,40 @@ const assert = require("assert");
 
 describe("Router", function () {
     beforeEach(function () {
-        this.router = new Router();
+    })
+    
+    it("can store one GET route", () => {
+        var router = new Router();
+        var callback = function () {};
+        router.route("GET", "/", callback);
+        
+        assert.deepEqual(router.routes["GET"], 
+            [
+                { 
+                    regexp: new RegExp("^/$", "i"),
+                    callback: callback
+                }
+            ]
+        )
+    })
+
+    it("can store multiple GET routes", () => {
+        var router = new Router();
+        var callback = function () {};
+        router.route("GET", "/", callback);
+        router.route("GET", "/home", callback);
+        
+        assert.deepEqual(router.routes["GET"], 
+            [
+                { 
+                    regexp: new RegExp("^/$", "i"),
+                    callback: callback
+                },
+                { 
+                    regexp: new RegExp("^/home$", "i"),
+                    callback: callback
+                }
+            ]
+        )
     })
 })
