4a2944c65f5e022b4bc7c1beeda540c9114be9df
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-03-15 12:24:58 +0530

    Init repo and init mocha test

---
+++ test/index.js
@@ -0,0 +1,8 @@
+const Router = require ("../lib");
+const assert = require("assert");
+
+describe("Router", function () {
+    beforeEach(function () {
+        this.router = new Router();
+    })
+})
