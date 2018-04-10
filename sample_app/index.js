fef8b9e637a495ce9f858d010f3df8e6771da964
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-04-10 12:50:14 +0530

    Added response object

--- sample_app/index.js
+++ sample_app/index.js
@@ -2,15 +2,24 @@ let rocket = require('../lib/rocket');
 
 let app = rocket();
 
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    console.log(`Fetching...`);
+    console.log(`Fetching...${req.url}`);
+    res.render('index', {title: 'Our own MVC Framework'});
+});
+
+app.get('/aboutus', (req, res) => {
+    console.log(`Fetching...${req.url}`);
+    res.render('aboutus', {text: 'An agile startup'});
 });
 
 app.get('/greeting/:message', (req, res) => {
     console.log(`Welcome...${req.url} -> ${req.message}`);
 });
 
-app.get('/send/:message/:bye', (req, res) => {
+app.get('/send/:message/:to', (req, res) => {
     console.log(`Welcome...${req.url} -> ${req.message} -> -> ${req.bye}`);
 });
 
