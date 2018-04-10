eda03ec7d53b307098ff7ac812d4d0f1d347dd20
Rajesh Pillai <pillai.rajesh@gmail.com>
2018-04-10 12:11:15 +0530

    Refactor routes and app

--- sample_app/index.js
+++ sample_app/index.js
@@ -7,8 +7,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/greeting/:message', (req, res) => {
-    console.log(`Welcome...${req.url}`);
+    console.log(`Welcome...${req.url} -> ${req.message}`);
 });
+
+app.get('/send/:message/:bye', (req, res) => {
+    console.log(`Welcome...${req.url} -> ${req.message} -> -> ${req.bye}`);
+});
+
 
 
 //console.log(app.router.routes);
